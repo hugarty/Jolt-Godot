e2db745dd961dd204e98ecad03f2150395579965
hugarty <hp-ca@hotmail.com>
2025-03-06 08:17:49 -0300

    Linked well

--- GITHUB_HELP2/testdebugger/main.cpp
+++ GITHUB_HELP2/testdebugger/main.cpp
@@ -1,12 +1,67 @@
 #include <Jolt/Jolt.h>
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+
 #include <iostream>
+#include <cstdarg>
 
 #include <Application/Application.h>
 #include <Renderer/Renderer.h>
+
+
 using String = std::basic_string<char, std::char_traits<char>, STLAllocator<char>>;
 
 
+// Callback for traces, connect this to your own trace function if you have one
+static void TraceImpl(const char *inFMT, ...)
+{
+	// Format the message
+	va_list list;
+	va_start(list, inFMT);
+	char buffer[1024];
+	vsnprintf(buffer, sizeof(buffer), inFMT, list);
+	va_end(list);
+
+	// Print to the TTY
+	cout << buffer << endl;
+}
+
+#ifdef JPH_ENABLE_ASSERTS
+
+// Callback for asserts, connect this to your own assert handler if you have one
+static bool AssertFailedImpl(const char *inExpression, const char *inMessage, const char *inFile, uint inLine)
+{
+	// Print to the TTY
+	cout << inFile << ":" << inLine << ": (" << inExpression << ") " << (inMessage != nullptr? inMessage : "") << endl;
+
+	// Breakpoint
+	return true;
+};
+
+#endif // JPH_ENABLE_ASSERTS
+
+
 int main(int argc, char **argv) {
+    std::cout << "oi" << std::endl;
+
+	// Register allocation hook. In this example we'll just let Jolt use malloc / free but you can override these if you want (see Memory.h).
+	// This needs to be done before any other Jolt function is called.
+	RegisterDefaultAllocator();
+
+	// Install trace and assert callbacks
+	Trace = TraceImpl;
+	JPH_IF_ENABLE_ASSERTS(AssertFailed = AssertFailedImpl;)
+
+	// Create a factory, this class is responsible for creating instances of classes based on their name or hash and is mainly used for deserialization of saved data.
+	// It is not directly used in this example but still required.
+	JPH::Factory::sInstance = new JPH::Factory();
+
+	// Register all physics types with the factory and install their collision handlers with the CollisionDispatch class.
+	// If you have your own custom shape types you probably need to register their handlers with the CollisionDispatch before calling this function.
+	// If you implement your own default material (PhysicsMaterial::sDefault) make sure to initialize it before this function or else this function will create one for you.
+	RegisterTypes();
+
+
     Application debugRenderApplication("ContatoComChao", String());
     debugRenderApplication.Run();
     
